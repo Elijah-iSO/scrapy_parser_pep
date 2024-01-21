06e71ddce7ac3f6030f7278680fcb4639e671a90
Илья Олейников <oleynikovis@yandex.ru>
2024-01-21 16:37:09 +0300

    changes applied

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -7,12 +7,11 @@ from pep_parse.settings import BASE_DIR
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        results_dir = BASE_DIR / 'results'
-        results_dir.mkdir(exist_ok=True)
         timestapm = time.strftime("%Y-%m-%d_%H-%M-%S")
         file_name = f'status_summary_{timestapm}.csv'
-        file_path = results_dir / file_name
-        self.file = open(file_path, 'w', encoding='utf-8')
+        self.results_dir = BASE_DIR / 'results'
+        self.file_path = self.results_dir / file_name
+
         self.status_counter = defaultdict(int)
 
     def process_item(self, item, spider):
@@ -20,12 +19,16 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        results = [('Cтатус', 'Количество')]
         self.status_counter['Total'] = sum(self.status_counter.values())
 
-        for status, amount in self.status_counter.items():
-            results.append((status, amount))
+        results = [
+            ('Cтатус', 'Количество'),
+            *self.status_counter.items()
+        ]
 
-        writer = csv.writer(self.file, dialect='unix')
-        writer.writerows(results)
+        self.results_dir.mkdir(exist_ok=True)
+
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            writer = csv.writer(f, dialect='unix', quoting=csv.QUOTE_MINIMAL)
+            writer.writerows(results)
         self.file.close()
